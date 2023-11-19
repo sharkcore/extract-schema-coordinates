0a2ab2d3e1dd21edad811f41ed73ce3be8db53e9
Mark Larah <mark@larah.me>
2023-11-19 11:31:46 -0600

    v3.0.6

--- strip-postinstall.sh
+++ strip-postinstall.sh
@@ -4,3 +4,4 @@ set -euo pipefail
 # prevent our postinstall hook from infecting applications 
 jq 'del(.scripts.postinstall)' package.json > package.json.tmp
 mv package.json.tmp package.json
+rm package.json.tmp
